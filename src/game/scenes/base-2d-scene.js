2d7924ca51f5c234212a9f4b25c94d99353983ee
Javiera <e285117@sbl.ch>
2025-04-10 07:12:31 +0000

    sunflower programmieren

--- src/game/scenes/base-2d-scene.js
+++ src/game/scenes/base-2d-scene.js
@@ -6,6 +6,8 @@ import Player from "../gameObjects/player/player"
 import NPC from "../gameObjects/player/npc"
 import Flowerpurple from "../gameObjects/pickups/flowerpurple"
 import FlowerWhite from "../gameObjects/pickups/flowerwhite"
+import sunflower from "../gameObjects/pickups/sunflower"
+import Sunflower from "../gameObjects/pickups/sunflower"
 
 /**
  * Erweiterung einer Phaser.Scene mit praktischen Funktionen um Spielobjekte
@@ -116,6 +118,7 @@ export default class Base2DScene extends Phaser.Scene {
       FlowerWhite,
       this.items,
     )
+    this.createObjects(this.map, "Items", "Sunflower", Sunflower, this.items)
   }
 
   createCamera() {
